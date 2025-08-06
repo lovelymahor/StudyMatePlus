fc711898a5166020cd9b01bea7a8618b4aa9223d
Sharanpreet-kaur <sharanpreetkaurngl@gmail.com>
2025-08-07 01:26:14 +0530

    Added Notes Section

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Syllabus from './pages/Syllabus';
+import Notes from './pages/Notes';
 import PYQs from './pages/PYQs';
 import Feedback from './pages/Feedback';
 import About from './pages/About';
@@ -16,6 +17,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/syllabus" element={<Syllabus />} />
+          <Route path="/notes" element={<Notes />} />
           <Route path="/pyqs" element={<PYQs />} />
           <Route path="/feedback" element={<Feedback />} />
           <Route path="/about" element={<About />} />
